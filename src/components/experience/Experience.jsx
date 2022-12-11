6c8fb314000df14069cdd5fbe96240f8a736f48c
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2022-12-11 14:16:22 +0530

    changes in Experience component

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -102,7 +102,7 @@ const Experience = () => {
             <article className='experience__details'>
               <BsPatchCheckFill className='experience__details-icon'/>
               <div>
-                <h4>VERCAL</h4>
+                <h4>VERCEL</h4>
                 <small className='text-light' >Intermediate</small>
               </div>
             </article>
