21ca389488e440c5ad4c72119368e199df7a6f25
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-08-23 20:40:53 +0530

    experience section improved

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,52 +1,61 @@
-import React from 'react'
-import "./about.css"
-import ME from '../../assets/me-about.jpeg'
-import {FaAward} from 'react-icons/fa'
-import {FiUsers} from 'react-icons/fi'
-import {VscFolderLibrary} from 'react-icons/vsc'
+import React from "react";
+import "./about.css";
+import ME from "../../assets/me-about.jpeg";
+import { FaAward } from "react-icons/fa";
+import { FiUsers } from "react-icons/fi";
+import { VscFolderLibrary } from "react-icons/vsc";
 
 const About = () => {
   return (
-  <section id='about' >
-    <h5>Get to Know</h5>
-    <h2>About Me</h2>
+    <section id="about">
+      <h5>Get to Know</h5>
+      <h2>About Me</h2>
 
-    <div className='container about__container'>
-      <div className='about__me'>
-        <div className='about__me-image' >
-          <img src={ME} alt='About Image'/>
+      <div className="container about__container">
+        <div className="about__me">
+          <div className="about__me-image">
+            <img src={ME} alt="About Image" />
+          </div>
+        </div>
+        <div className="about__content">
+          <div className="about__cards">
+            <article className="about__card">
+              <FaAward className="about__icon" />
+              <h5>Experience</h5>
+              <small>12+ Months</small>
+            </article>
+            <article className="about__card">
+              <FiUsers className="about__icon" />
+              <h5>Clients</h5>
+              <small>1000+ Worldwide</small>
+            </article>
+            <article className="about__card">
+              <VscFolderLibrary className="about__icon" />
+              <h5>Projects</h5>
+              <small>10+ Completed</small>
+            </article>
+          </div>
+
+          <p>
+            I am a Full Stack Developer with a passion for building beautiful
+            and user-friendly websites. I have strong understanding of both
+            front-end and back-end development, and I am excited to put my
+            skills to use in a professional setting. I am a quick learner and a
+            team player, and I am confident that I can be a valuable asset to
+            any Web Development Team
+          </p>
+
+          <a
+            href="#contact"
+            className="btn btn-primary"
+            style={{ marginTop: "10px" }}
+          >
+            Let's Talk
+          </a>
         </div>
       </div>
-      <div className='about__content'>
-        <div className='about__cards'>
-          <article className='about__card'>
-            <FaAward className='about__icon'/>
-            <h5>Experience</h5>
-            <small>5+ Months</small>
-          </article>
-          <article className='about__card'>
-            <FiUsers className='about__icon'/>
-            <h5>Clients</h5>
-            <small>1000+ Worldwide</small>
-          </article>
-          <article className='about__card'>
-            <VscFolderLibrary className='about__icon'/>
-            <h5>Projects</h5>
-            <small>5+ Completed</small>
-          </article>
-        </div>
+    </section>
+  );
+};
 
-        <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras vel nibh mauris. Aliquam eget augue sed diam sodales pharetra. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Ut erat risus, placerat in blandit at, tincidunt sit amet purus. Integer ut pretium orci. Mauris pretium nisi in molestie 
-        </p>
-
-        <a href='#contact' className='btn btn-primary'>Let's Talk</a>
-
-      </div>
-    </div>
-
-  </section>
-  )
-}
-
-export default About
+export default About;
--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -1,118 +1,117 @@
-import React from 'react'
-import "./experience.css"
-import {BsPatchCheckFill} from 'react-icons/bs'
+import React from "react";
+import "./experience.css";
+import { BsPatchCheckFill } from "react-icons/bs";
 
+const frontend = [
+  {
+    title: "HTML3",
+    experience: "Experienced",
+  },
+  {
+    title: "CSS5",
+    experience: "Experienced",
+  },
+  {
+    title: "JAVASCRIPT",
+    experience: "Intermediate",
+  },
+  {
+    title: "TAILWIND",
+    experience: "Experienced",
+  },
+  {
+    title: "REACTJS",
+    experience: "Experienced",
+  },
+  {
+    title: "REDUX TOOLKIT",
+    experience: "Experienced",
+  },
+  {
+    title: "TYPESCRIPT",
+    experience: "Experienced",
+  },
+  {
+    title: "NEXTJS",
+    experience: "Experienced",
+  },
+];
 
+const otherTech = [
+  {
+    title: "NODEJS",
+    experience: "Intermediate",
+  },
+  {
+    title: "EXPRESSJS",
+    experience: "Intermediate",
+  },
+  {
+    title: "MONGO DB",
+    experience: "Experienced",
+  },
+  {
+    title: "GITHUB",
+    experience: "Experienced",
+  },
+  {
+    title: "AZURE DEVOPS",
+    experience: "Intermediate",
+  },
+  {
+    title: "FIREBASE",
+    experience: "Intermediate",
+  },
+  {
+    title: "POSTMAN",
+    experience: "Experienced",
+  },
+];
 const Experience = () => {
   return (
-    <section id='experience'>
+    <section id="experience">
       <h5>What Skills I Have</h5>
       <h2>My Experience</h2>
 
-      <div className='container experience__container' >
-        <div className='experience__frontend' >
+      <div className="container experience__container">
+        <div className="experience__frontend">
           <h3>Frontend Development</h3>
-          <div className='experience__content' >
-            <article className='experience__details'>
-              <BsPatchCheckFill className='experience__details-icon'/>
-              <div>
-                <h4>HTML5</h4>
-                <small className='text-light' >Experienced</small>
-              </div>
-            </article>
-            <article className='experience__details'>
-              <BsPatchCheckFill className='experience__details-icon'/>
-              <div>
-                <h4>CSS3</h4>
-                <small className='text-light' >Experienced</small>
-              </div>
-            </article>
-            <article className='experience__details'>
-              <BsPatchCheckFill className='experience__details-icon'/>
-              <div>
-                <h4>JAVASCRIPT</h4>
-                <small className='text-light' >Intermediate</small>
-              </div>
-            </article>
-            <article className='experience__details'>
-              <BsPatchCheckFill className='experience__details-icon'/>
-              <div>
-                <h4>TAILWIND</h4>
-                <small className='text-light' >Experienced</small>
-              </div>
-            </article>
-            <article className='experience__details'>
-              <BsPatchCheckFill className='experience__details-icon'/>
-              <div>
-                <h4>REACTJS</h4>
-                <small className='text-light' >Intermediate</small>
-              </div>
-            </article>
-            <article className='experience__details'>
-              <BsPatchCheckFill className='experience__details-icon'/>
-                <div>
-                  <h4>GITHUB</h4>
-                  <small className='text-light' >Experienced</small>
-                </div>
-            </article>
-           
+          <div className="experience__content">
+            {frontend.map((item, index) => {
+              return (
+                <article className="experience__details" key={index}>
+                  <BsPatchCheckFill className="experience__details-icon" />
+                  <div>
+                    <h4>{item.title}</h4>
+                    <small className="text-light">{item.experience}</small>
+                  </div>
+                </article>
+              );
+            })}
           </div>
         </div>
 
-       { /* Frontend Part End*/}
-        
-        <div className='experience__technologies' >
-          <h3>OTHER TECHNOLOGIES</h3>
-            <div className='experience__content' >
-            <article className='experience__details'>
-              <BsPatchCheckFill className='experience__details-icon'/>
-              <div>
-                <h4>C LANGUAGE</h4>
-                <small className='text-light' >Intermediate</small>
-              </div>
-            </article>
-            <article className='experience__details'>
-              <BsPatchCheckFill className='experience__details-icon'/>
-              <div>
-                <h4>C++</h4>
-                <small className='text-light' >Experienced</small>
-              </div>
-            </article>
-            <article className='experience__details'>
-              <BsPatchCheckFill className='experience__details-icon'/>
-              <div>
-                <h4>JAVA</h4>
-                <small className='text-light' >Intermediate</small>
-              </div>
-            </article>
-            <article className='experience__details'>
-              <BsPatchCheckFill className='experience__details-icon'/>
-              <div>
-                <h4>DATA STRUCTURE</h4>
-                <small className='text-light' >Intermediate</small>
-              </div>
-            </article>
-            <article className='experience__details'>
-              <BsPatchCheckFill className='experience__details-icon'/>
-              <div>
-                <h4>FIGMA</h4>
-                <small className='text-light' >Beginner</small>
-              </div>
-            </article>
-            <article className='experience__details'>
-              <BsPatchCheckFill className='experience__details-icon'/>
-              <div>
-                <h4>VERCEL</h4>
-                <small className='text-light' >Intermediate</small>
-              </div>
-            </article>
-            </div>
+        {/* Frontend Part End*/}
+
+        <div className="experience__technologies">
+          <h3>Other Technologies</h3>
+          <div className="experience__content">
+            {otherTech.map((item, index) => {
+              return (
+                <article className="experience__details" key={index}>
+                  <BsPatchCheckFill className="experience__details-icon" />
+                  <div>
+                    <h4>{item.title}</h4>
+                    <small className="text-light">{item.experience}</small>
+                  </div>
+                </article>
+              );
+            })}
+          </div>
         </div>
-
       </div>
-
     </section>
-  )
-}
+  );
+};
 
-export default Experience
+export default Experience;
